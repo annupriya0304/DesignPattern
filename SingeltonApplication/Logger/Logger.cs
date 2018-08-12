cfbcfc3721a57986d203bd836d8fe3fd95d66de8
annpriya <annupriya0304@gmail.com>
2018-08-12 12:27:39 +0530

    Added new project

---
+++ SingeltonApplication/Logger/Logger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace SingeltonApplication.Logger
+{
+    public sealed class Logger : Ilog
+    {
+        private static Lazy<Logger> Instance = new Lazy<Logger>(() => new Logger());
+
+        public static Logger GetInstance
+        {
+            get
+            {
+                return Instance.Value;
+            }
+        }
+
+        private Logger()
+        {
+
+        }
+
+        public void LogException(string Message)
+        {
+            string fileName = string.Format("{0}_{1}.log", "Exception", DateTime.Now.ToShortDateString());
+            string logFilePath = string.Format(@"{0}\{1}", AppDomain.CurrentDomain.BaseDirectory, fileName);
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("----------------------------------------");
+            sb.AppendLine(DateTime.Now.ToString());
+            sb.AppendLine(Message);
+            using (StreamWriter writer = new StreamWriter(logFilePath, true))
+            {
+                writer.Write(sb.ToString());
+                writer.Flush();
+            }
+        }
+    }
+}
