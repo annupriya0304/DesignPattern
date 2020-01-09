2acf69f15349902372c349458ea83374985b513d
Annu Priya <annupriya0304@gmail.com>
2020-01-09 10:49:16 +0530

    pushed changes

---
+++ codechef1/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Result
+{
+
+    /*
+     * Complete the 'processStrings' function below.
+     *
+     * The function is expected to return a STRING_ARRAY.
+     * The function accepts following parameters:
+     *  1. STRING str1
+     *  2. STRING str2
+     */
+
+    public static List<string> processStrings(string str1, string str2)
+    {
+        char[] str1chararray = str1.ToCharArray();
+        char[] str2charrarray = str2.ToCharArray();
+        var diff1 = string.Join("", str1chararray.Except(str2charrarray));
+        var diff2 = string.Join("", str2charrarray.Except(str1chararray));
+
+        var ans = new List<string>();
+        ans.Add(diff1);
+        ans.Add(diff2);
+        return ans;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        string str1 = Console.ReadLine();
+
+        string str2 = Console.ReadLine();
+
+        List<string> result = Result.processStrings(str1, str2);
+        
+    }
+}
--- grid/Program.cs
+++ grid/Program.cs
@@ -1,62 +1,31 @@
-using System.CodeDom.Compiler;
+using System;
 using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
 
-class Solution
+namespace Solution
 {
-
-    // Complete the gridChallenge function below.
-    static string gridChallenge(string[] grid)
+    class Solution
     {
-        string[] column = new string[grid.Length];
-        for (int i = 0; i < grid.Length; i++)
+        static void Main(string[] args)
         {
-            for (int j = 0; j < grid.Length; j++)
+            var line = Console.ReadLine();
+            var words = line.Split(' ').ToArray();
+            int i = 0;
+            foreach(var word in words)
             {
-
-                column[i] += grid[j][i];
-            }
-          var sorted = new string(column[i].OrderBy(c => c).ToArray());
-            if (column[i] != sorted)
-                return "NO";
-
-        }
-        return "YES";
-
-    }
-
-    static void Main(string[] args)
-    {
-
-        int t = Convert.ToInt32(Console.ReadLine());
-
-        for (int tItr = 0; tItr < t; tItr++)
-        {
-            int n = Convert.ToInt32(Console.ReadLine());
-
-            string[] grid = new string[n];
-
-            for (int i = 0; i < n; i++)
-            {
-                string gridItem = Console.ReadLine();
-                grid[i] = gridItem;
-                grid[i] = new string(grid[i].OrderBy(c => c).ToArray());
+                var tcount = word.Count(x => x == 't');
+                if(tcount==1 || tcount==3)
+                {
+                    i++;
+                    Console.WriteLine(string.Join("",word.ToCharArray().Reverse()));
+                }
             }
 
-            string result = gridChallenge(grid);
-            Console.WriteLine(result);
-            
+            if(i==0)
+            {
+                Console.WriteLine("None");
+            }
         }
-        
     }
 }
